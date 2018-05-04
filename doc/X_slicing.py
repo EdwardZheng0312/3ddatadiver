1b62590deb00904591290b0dd4ecd62e1b457704
EdwardZheng0312 <37917464+EdwardZheng0312@users.noreply.github.com>
2018-05-03 17:13:29 -0700

    Update X_slicing.py

--- doc/X_slicing.py
+++ doc/X_slicing.py
@@ -3,8 +3,8 @@ import pandas as pd
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
-import load_data
 import create_pslist
+import load_data
 
 
 def x_slicing(filename, Z_direction, X, x_actual, y_actual, x_size, y_size):
