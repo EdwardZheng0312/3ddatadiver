8735be060b200ca3cb1e58b8b0edff9841c14515
EdwardZheng0312 <37917464+EdwardZheng0312@users.noreply.github.com>
2018-05-03 17:15:29 -0700

    Update XY_Slicing_Animation.py

--- doc/XY_Slicing_Animation.py
+++ doc/XY_Slicing_Animation.py
@@ -3,8 +3,8 @@ import pandas as pd
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
-import load_data
 import create_pslist
+import load_data
 
 
 def xy_slicing_animation(filename, Z_direction, number_slices, x_actual, y_actual, x_size, y_size):
