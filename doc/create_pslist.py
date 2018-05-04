591f50459e83d0ad6be623f92e2af1a7773efe68
EdwardZheng0312 <37917464+EdwardZheng0312@users.noreply.github.com>
2018-05-03 17:11:15 -0700

    Update create_pslist.py

--- doc/create_pslist.py
+++ doc/create_pslist.py
@@ -1,9 +1,11 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
+
 import load_data
 
+
 def create_pslist(filename, x_size, y_size):
-    """The function for reshape the input data file depends on certain shape of the input data file""""
+    """The function for reshape the input data file depends on certain shape of the input data file"""
     data, z, z_approach, z_retract = load_data.load_data(filename)
     #phase shift
     pslist = []
