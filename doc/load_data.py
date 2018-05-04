d3f028b9f92bbc822921cea7e28454abc86f3c64
EdwardZheng0312 <37917464+EdwardZheng0312@users.noreply.github.com>
2018-05-03 17:12:35 -0700

    Update load_data.py

--- doc/load_data.py
+++ doc/load_data.py
@@ -1,18 +1,9 @@
-
-# coding: utf-8
-
-# In[4]:
-
-
 import pandas as pd
 
-
-# In[5]:
-# Call the function like this: pslist,z_approach = data('PHASEdata.csv')
-
 def load_data(filename):
+    """The function to do the primary data clean process on the input data file."""
     data = pd.read_csv(filename)
     z = data.iloc[:,0]
     z_approach = z[:500]
     z_retract = z[500:]
-    return data,z,z_approach,z_retract
+    return data, z, z_approach, z_retract
