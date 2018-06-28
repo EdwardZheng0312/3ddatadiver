ee943f13a086867158c5a4f3645fd4031b3b27e9
Emurphy2028 <35085990+Emurphy2028@users.noreply.github.com>
2018-06-28 15:16:42 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_packages
 import os
+PACKAGES = find_packages()
 # Get version and release info, which is all stored in 3ddatadiver/version.py
 #ver_file = os.path.join('3ddatadiver', 'version.py')
 #with open(ver_file) as f:
@@ -12,26 +13,49 @@ AUTHOR_EMAIL = "renloz@uw.edu; emurphy2028@gmail.com; xueqiaozhang@hotmail.com"
 URL = "https://github.com/EdwardZheng0312/VisualanalysisAFM"
 VERSION = "0.10"
 PACKAGES = "3ddatadiver"
-setup(
-    name=NAME,
-    version=VERSION,
-    description=DESCRIPTION,
-    author=AUTHOR,
-    author_email=AUTHOR_EMAIL,
-    license="BSD",
-    url=URL,
-    packages=PACKAGES,
-   
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Environment :: Web Environment",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Framework :: Django",
-    ],
-    zip_safe=False,
-    )
+opts = dict(name=NAME,
+            maintainer=MAINTAINER,
+            maintainer_email=MAINTAINER_EMAIL,
+            description=DESCRIPTION,
+            long_description=LONG_DESCRIPTION,
+            url=URL,
+            download_url=DOWNLOAD_URL,
+            license=LICENSE,
+            classifiers=CLASSIFIERS,
+            author=AUTHOR,
+            author_email=AUTHOR_EMAIL,
+            platforms=PLATFORMS,
+            version=VERSION,
+            packages=PACKAGES,
+            package_data=PACKAGE_DATA,
+            # install_requires=REQUIRES,
+            include_package_data=True,
+            install_requires=REQUIRES)
+
+
 if __name__ == '__main__':
     setup(**opts)
+    
+#setup(
+#    name=NAME,
+#    version=VERSION,
+#    description=DESCRIPTION,
+#    author=AUTHOR,
+#    author_email=AUTHOR_EMAIL,
+#    license="BSD",
+#    url=URL,
+#    packages=PACKAGES,
+   
+ #   classifiers=[
+ #       "Development Status :: 3 - Alpha",
+ #       "Environment :: Web Environment",
+ #       "Intended Audience :: Developers",
+ #       "License :: OSI Approved :: BSD License",
+ #       "Operating System :: OS Independent",
+ #       "Programming Language :: Python",
+ #       "Framework :: Django",
+  #  ],
+  #  zip_safe=False,
+  #  )
+#if __name__ == '__main__':
+#    setup(**opts)
