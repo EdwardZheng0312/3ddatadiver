cfc0e530e03722afb72bcf8a834f4fe710d54c51
Emurphy2028 <35085990+Emurphy2028@users.noreply.github.com>
2018-06-28 15:30:01 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 import os
 PACKAGES = find_packages()
 # Get version and release info, which is all stored in 3ddatadiver/version.py
-#ver_file = os.path.join('3ddatadiver', 'version.py')
-#with open(ver_file) as f:
-#    exec(f.read())
+ver_file = os.path.join('3ddatadiver', 'version.py')
+with open(ver_file) as f:
+    exec(f.read())
 
 NAME = "3ddatadiver"
 DESCRIPTION = "AFM 3d_data_diver"
@@ -14,20 +14,20 @@ URL = "https://github.com/EdwardZheng0312/VisualanalysisAFM"
 VERSION = "0.10"
 #PACKAGES = "3ddatadiver"
 opts = dict(name=NAME,
-            #maintainer=MAINTAINER,
-            #maintainer_email=MAINTAINER_EMAIL,
+            maintainer=MAINTAINER,
+            maintainer_email=MAINTAINER_EMAIL,
             description=DESCRIPTION,
-            #long_description=LONG_DESCRIPTION,
+            long_description=LONG_DESCRIPTION,
             url=URL,
-            #download_url=DOWNLOAD_URL,
-            #license=LICENSE,
-            #classifiers=CLASSIFIERS,
+            download_url=DOWNLOAD_URL,
+            license=LICENSE,
+            classifiers=CLASSIFIERS,
             author=AUTHOR,
-            #author_email=AUTHOR_EMAIL,
-            #platforms=PLATFORMS,
+            author_email=AUTHOR_EMAIL,
+            platforms=PLATFORMS,
             version=VERSION,
             packages=PACKAGES,
-            #package_data=PACKAGE_DATA,
+            package_data=PACKAGE_DATA,
             # install_requires=REQUIRES,
             include_package_data=True,
             install_requires=REQUIRES)
