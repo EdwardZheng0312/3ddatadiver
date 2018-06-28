7829bf039c5592283c8e9c6590d004b703baec6a
Ellen Murphy <emurphy89@uw.edu>
2018-06-28 14:16:49 -0700

    Edited unittests

--- 3DdataDiver/tests/test_correct_slope.py
+++ 3DdataDiver/tests/test_correct_slope.py
@@ -1,14 +1,4 @@
-import unittest
-
-
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
-
-
-if __name__ == '__main__':
-    unittest.main()
-
+import numpy as np
 
 def test_correct_slope():
     test1 = np.random.random_integers(0, 9 + 1, size=(500, 20, 20))
