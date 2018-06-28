799162b64a74787d25b84e0cc59a4a03a60421a0
Ellen Murphy <emurphy89@uw.edu>
2018-06-28 14:18:23 -0700

    Edited unittest

--- 3DdataDiver/tests/test_generate_array.py
+++ 3DdataDiver/tests/test_generate_array.py
@@ -1,14 +1,3 @@
-import unittest
-
-
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
-
-
-if __name__ == '__main__':
-    unittest.main()
-
 def test_generate_array():
     test1 = np.random.random_integers(0, 9 + 1, size=(500, 20, 20))
     try:
