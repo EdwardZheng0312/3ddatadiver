d4c95b85d1fd556382547bb49e8f80055104a789
Emurphy2028 <35085990+Emurphy2028@users.noreply.github.com>
2018-09-26 15:04:13 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,38 +13,41 @@ with open(ver_file) as f:
 #URL = "https://github.com/EdwardZheng0312/VisualanalysisAFM"
 #VERSION = "0.10"
 #PACKAGES = "3ddatadiver"
-opts = dict(name=NAME,
-            maintainer=MAINTAINER,
-            maintainer_email=MAINTAINER_EMAIL,
-            description=DESCRIPTION,
-            long_description=LONG_DESCRIPTION,
-            url=URL,
-            download_url=DOWNLOAD_URL,
-            license=LICENSE,
-            classifiers=CLASSIFIERS,
-            author=AUTHOR,
-            author_email=AUTHOR_EMAIL,
-            platforms=PLATFORMS,
-            version=VERSION,
-            packages=PACKAGES,
-            package_data=PACKAGE_DATA,
-            # install_requires=REQUIRES,
-            include_package_data=True,
-            install_requires=REQUIRES)
+#opts = dict(name=NAME,
+ #           maintainer=MAINTAINER,
+ #           maintainer_email=MAINTAINER_EMAIL,
+ #           description=DESCRIPTION,
+ #           long_description=LONG_DESCRIPTION,
+ #           url=URL,
+ #           download_url=DOWNLOAD_URL,
+ #           license=LICENSE,
+ #           classifiers=CLASSIFIERS,
+ #           author=AUTHOR,
+ #           author_email=AUTHOR_EMAIL,
+ #           platforms=PLATFORMS,
+ #           version=VERSION,
+ #           packages=PACKAGES,
+ #           package_data=PACKAGE_DATA,
+ #           # install_requires=REQUIRES,
+ #           include_package_data=True,
+ #           install_requires=REQUIRES)
 
 
-if __name__ == '__main__':
-    setup(**opts)
+#if __name__ == '__main__':
+#    setup(**opts)
     
-#setup(
-#    name=NAME,
-#    version=VERSION,
-#    description=DESCRIPTION,
-#    author=AUTHOR,
-#    author_email=AUTHOR_EMAIL,
-#    license="BSD",
-#    url=URL,
-#    packages=PACKAGES,
+setup(
+    name='3ddatadiver',
+    version='0.1',
+    description='3D AFM data visualization tool.',
+    author='Xueqiao Zhang, Renlong Zheng, Ellen Murphy',
+    author_email='emurphy2028@gmail.com',
+    license="MIT",
+    url='https://github.com/EdwardZheng0312/3ddatadiver',
+    packages=PACKAGES,
+    install_requires=[certifi==2018.4.16, cycler==0.10.0, h5py==2.8.0, kiwisolver==1.0.1, matplotlib==2.2.2, numpy==1.14.5, 
+                      pandas==0.23.1, pyparsing==2.2.0, python-dateutil==2.7.3, pytz==2018.4, six==1.11.0, wincertstore==0.2],
+    zip_safe=False)
    
  #   classifiers=[
  #       "Development Status :: 3 - Alpha",
